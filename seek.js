8663479d1d8cda5e3a512e58104dcad3811a84ad
Anggher Agusti <anggheragusti@gmail.com>
2019-01-28 20:29:44 +0700

    first commit

---
+++ seek.js
@@ -0,0 +1,11 @@
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-64994098-1';
+    document.head.appendChild(script);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-64994098-1');
+})();
