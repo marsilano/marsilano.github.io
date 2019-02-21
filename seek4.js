07ab8086d9f4409e2efd37ab9128e756bf9f1536
Anggher Sukma Agusti <anggheragusti@gmail.com>
2019-02-21 12:56:07 +0700

    add seek4

---
+++ seek4.js
@@ -0,0 +1,1 @@
+(function () {var script = document.createElement('script');script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-64994098-4';document.head.appendChild(script);window.dataLayer = window.dataLayer || [];function gtag() {dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-64994098-4');})();
