41645774377dac9b1fe1a179861de7bc0a26c9ba
Anggher Agusti <anggheragusti@gmail.com>
2019-04-21 09:17:26 +0700

    add seek5.js

---
+++ seek5.js
@@ -0,0 +1,1 @@
+(function () {var script = document.createElement('script');script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-64994098-5';document.head.appendChild(script);window.dataLayer = window.dataLayer || [];function gtag() {dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-64994098-5');})();
